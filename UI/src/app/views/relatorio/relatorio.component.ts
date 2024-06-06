c453b1c21a76149e5ad020b8c6be82754915c39b
lisa <heloisamedeiros7@hotmail.com>
2024-06-06 14:22:41 -0300

    tabela de relatorio feita + pag de relatorio feita

--- UI/src/app/views/relatorio/relatorio.component.ts
+++ UI/src/app/views/relatorio/relatorio.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { InfoBarComponent } from '../../components/info-bar/info-bar.component';
 import { TableRelatorioComponent } from '../../components/table-relatorio/table-relatorio.component';
+import { InputsComponent } from '../../components/inputs/inputs.component';
+import { ButtonsComponent } from '../../components/buttons/buttons.component';
 
 @Component({
   selector: 'relatorioView',
   standalone: true,
-  imports: [HeaderComponent, InfoBarComponent, TableRelatorioComponent],
+  imports: [HeaderComponent, InfoBarComponent, TableRelatorioComponent, InputsComponent, ButtonsComponent],
   templateUrl: './relatorio.component.html',
   styleUrl: './relatorio.component.css'
 })
