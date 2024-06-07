719734783c7e9eb50ed446ccdf5cb2161c4580e5
lisa <heloisamedeiros7@hotmail.com>
2024-06-07 17:15:16 -0300

    removendo comentarios inuteis

--- UI/src/app/components/buttons/buttons.component.ts
+++ UI/src/app/components/buttons/buttons.component.ts
@@ -14,10 +14,4 @@ export class ButtonsComponent {
   @Input() variant : btnVariant = "neutro";
   @Input("text") "text" : string = "";
   @Input("type") "type" : string = "";
-
-  // @Output() loginEvent = new EventEmitter<{valorinput : string | number}>();
-
-  // event(valorinput : string | number){
-  //   this.loginEvent.emit({valorinput})
-  // }
 }
