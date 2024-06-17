198666744b6a43f8a9ed1f03287bdb3e59dd4662
Kaique Bahmad <kaiquebahmadt@gmail.com>
2024-06-17 01:59:14 -0300

    validador cpf

--- API/src/main/java/com/bamboobyte/API/utils/Validador.java
+++ API/src/main/java/com/bamboobyte/API/utils/Validador.java
@@ -8,7 +8,33 @@ import java.util.Date;
 
 public class Validador {
     public static boolean isCpfValido(long cpf) {
-        return true;
+        String cpfString = String.valueOf(cpf).replaceAll("L", "");
+
+        if (cpfString.length() != 11) {
+            return false;
+        }
+
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += Character.getNumericValue(cpfString.charAt(i)) * (10 - i);
+        }
+        int digito1 = 11-(soma % 11);
+        if (digito1 == 10 || digito1 == 11) {
+            digito1 = 0;
+        }
+
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += Character.getNumericValue(cpfString.charAt(i)) * (11-i);
+        }
+
+        int digito2 = 11-(soma % 11);
+        if (digito2 == 10 || digito2 == 11) {
+            digito2 = 0;
+        }
+
+        return digito1 == Character.getNumericValue(cpfString.charAt(9)) &&
+                digito2 == Character.getNumericValue(cpfString.charAt(10));
     }
     public static String converteData(Long timestamp) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
